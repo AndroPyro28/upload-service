64b1f8c572f03a602a43c81ae8e517e521c3be25
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-04-16 16:12:02 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import { logger } from 'hono/logger'
 
 import { cors } from 'hono/cors'
 import { secureHeaders } from 'hono/secure-headers'
-
-const app = new Hono().basePath('/api')
+import { authMiddleware, TAuthVariables } from './middlewares/auth.middleware'
+const app = new Hono().basePath('/api/upload')
 .use('/api/*', cors())
 .use(secureHeaders())
 .use(logger())
+.use('*', authMiddleware)
 .get('/', async (c) => {
     return c.text("hello world upload", 200)
 })
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,11 +1,20 @@
 
 import { createMiddleware } from "hono/factory";
 export const authMiddleware = createMiddleware(async (c, next) => {
-  const user = c.get("authUser");
- 
+  const userId = c.req.header("x-user-id")
+
+  if(!userId) {
+    return c.json({
+      error: "Access denied! Please login to continue"
+    }, 401)
+  }
+
+  c.set('user', {userId})
   await next();
 });
 
 export type TAuthVariables = {
-  sample:string
+  user: {
+    userId: string
+  }
 };
